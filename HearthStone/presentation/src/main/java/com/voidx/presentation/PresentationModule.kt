9fe560611b0e849111c6f0a00242a63db2dd0165
Victor Fernandes <victorfernandes91@hotmail.com>
2020-07-15 12:16:01 -0300

    fixing view model declaration

--- HearthStone/presentation/src/main/java/com/voidx/presentation/PresentationModule.kt
+++ HearthStone/presentation/src/main/java/com/voidx/presentation/PresentationModule.kt
@@ -1,22 +1,21 @@
 package com.voidx.presentation
 
-import com.voidx.data.model.GameOption
-import com.voidx.presentation.dto.GameOptionDTO
+import com.voidx.presentation.filterResults.FilterResultsViewModel
+import com.voidx.presentation.filterResults.mapper.CardMapToResultDTO
 import com.voidx.presentation.list.ListGameInfoViewModel
 import com.voidx.presentation.list.mapper.GameOptionMapToGameOptionDto
-import com.voidx.presentation.mapper.Mapper
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import useCaseModule
 
 val module = module {
 
-    factory<Mapper<GameOption, GameOptionDTO>> {
-        GameOptionMapToGameOptionDto()
+    viewModel {
+        ListGameInfoViewModel(get(), get(), GameOptionMapToGameOptionDto())
     }
 
-    viewModel {
-        ListGameInfoViewModel(get(), get(), get())
+    viewModel { (category: String, type: String) ->
+        FilterResultsViewModel(category, type, get(), CardMapToResultDTO(), get())
     }
 
 }
