4175834ff932c506e0e9b391d33d133734435614
Victor Fernandes <victorfernandes91@hotmail.com>
2020-07-15 19:23:38 -0300

    created navigation util and updated old navigation to new

--- HearthStone/app/src/androidTest/java/com/voidx/hearthstone/list/ListGameOptionsRobot.kt
+++ HearthStone/app/src/androidTest/java/com/voidx/hearthstone/list/ListGameOptionsRobot.kt
@@ -1,31 +1,18 @@
 package com.voidx.hearthstone.list
 
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.navigation.Navigation
-import androidx.navigation.testing.TestNavHostController
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.voidx.hearthstone.R
 import com.voidx.hearthstone.feature.list.ListGameOptionsFragment
-import com.voidx.hearthstone.util.WaitFor
+import com.voidx.hearthstone.util.startNavigation
 import okhttp3.mockwebserver.MockWebServer
-import java.util.concurrent.TimeUnit
 
 class ListGameOptionsRobot(private val mockWebServer: MockWebServer) {
 
     fun start() = apply {
-        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
-        navController.setGraph(R.navigation.navigation)
-
-        // Create a graphical FragmentScenario for the TitleScreen
-        val scenario = launchFragmentInContainer<ListGameOptionsFragment>(themeResId = R.style.AppTheme)
-
-        // Set the NavController property on the fragment
-        scenario.onFragment { fragment ->
-            Navigation.setViewNavController(fragment.requireView(), navController)
-        }
+        startNavigation<ListGameOptionsFragment>()
     }
 
     fun checkTitle() = apply {
